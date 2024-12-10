e3248377b0a8ae4ea53713da8cbc5b179be6c73a
ninditaeka <ninditaeka@gmail.com>
2024-12-10 19:00:49 +0700

    change read me to read more

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
           href="/about"
           className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
         >
-          Read Me
+          Read More
         </Link>
       </div>
       <div className=" flex flex-row md:w-1/2 md:py-4 md:justify-end justify-center mb-4">
--- src/components/ListofPost.tsx
+++ src/components/ListofPost.tsx
@@ -93,7 +93,7 @@ export default function ListofPost({ posts }: any) {
                         href={"/blogs/" + post?.id}
                         className="inline-flex items-center  bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                       >
-                        Read Me
+                        Read More
                       </Link>
                     </div>
                   </Card>
